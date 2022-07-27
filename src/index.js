c9ac3f66a3d215e53c80a9dfdd5223de0f6706ae
lomon3 <lomon3@gmail.com>
2022-07-27 22:48:58 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+/* function check(str, bracketsConfig) {
+    for (let i = 0; i < str.length; i++) {
+        bracketsConfig.forEach(config => {
+            if (str[i] === config[0] && str[i + 1] === config[1]) {
+                str = str.slice(0, i) + str.slice(i + 2, str.length);
+                i -= 2;
+            }
+        });
+    }
+
+    return (str.length === 0);
+}
+const config5 = [
+    ['(', ')'],
+    ['|', '|']
+];
+
+console.log(check('|()|', config5));
+ */
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < str.length; i++) {
+        bracketsConfig.forEach(config => {
+            if (str[i] === config[0] && str[i + 1] === config[1]) {
+                str = str.slice(0, i) + str.slice(i + 2, str.length);
+                i -= 2;
+            }
+        });
+    }
+
+    return (str.length === 0);
 }
